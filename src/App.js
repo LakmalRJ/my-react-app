5767beb50938368b866a35b7210f406277180617
LakmalRJ <chlakmal@gmail.com>
2024-09-06 00:52:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 const styles = {
   container: {
     maxWidth: '800px',
-    margin: '10px auto',
+    margin: '200px auto',
     padding: '45px',
     backgroundColor: '#ffffff',
     borderRadius: '15px',
